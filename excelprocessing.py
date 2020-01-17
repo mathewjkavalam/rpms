a102929e5afb32ceb7c9849807d1b374555a46ba
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2020-01-17 10:47:42 +0530

    cells without value gives None

--- excelprocessing.py
+++ excelprocessing.py
@@ -1,13 +1,20 @@
 from openpyxl import load_workbook
 FILENAME = "timetableguide.xlsx"
-SHEETNAME = "Faculty _TT "
+SHEETNAME = "Sheet1"
 workbook = load_workbook(filename=FILENAME)
 #print("#",workbook.sheetnames)
 worksheet = workbook[SHEETNAME]
 """
 merged row cells need to take the starting row
 to retrieve value;otherwise its none.
+output:
+# S1(D3-G3)
+# None
+# None
+# None
 """
 COLUMN = 1
-ROW = 5
-print("#",worksheet.cell(column=COLUMN,row=ROW).value)
+ROW = 3
+#print("#",worksheet.cell(column=COLUMN,row=ROW).value)
+for COLUMN in range(4,7+1):
+    print("#",worksheet.cell(column=COLUMN,row=ROW).value)
