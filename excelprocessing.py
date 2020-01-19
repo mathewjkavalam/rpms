d0109a82393a89d249c620c6bb8639256965143c
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2020-01-20 02:44:58 +0530

    columns and rows identified in standard tt

--- excelprocessing.py
+++ excelprocessing.py
@@ -13,8 +13,27 @@ output:
 # None
 # None
 """
-COLUMN = 1
-ROW = 3
+#COLUMN = 1
+#ROW = 3
 #print("#",worksheet.cell(column=COLUMN,row=ROW).value)
-for COLUMN in range(4,7+1):
-    print("#",worksheet.cell(column=COLUMN,row=ROW).value)
+name = (1,1)
+
+mon = 3
+tue = 5
+wed = 7
+thu = 9
+fri = 11
+
+COLUMN = 1
+ROW = 0
+
+classhours = [0,3,4,6,7,9,11,12]
+#for COLUMN in range(4,7+1):
+#    print("#",worksheet.cell(column=COLUMN,row=ROW).value)
+"""
+Sample INPUT:
+(fri,5)
+Sample OUTPUT:
+S7
+"""
+print("#",worksheet.cell(column=classhours[5],row=fri).value)
