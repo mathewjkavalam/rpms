bb6ceed2ebfaa6e2312463b8cbdb01ef70e5b227
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2020-01-24 16:31:53 +0530

    Guides in a single file handled

--- excelprocessingMultipleFiles.py
+++ excelprocessingMultipleFiles.py
@@ -1,4 +1,21 @@
 from openpyxl import load_workbook
+def isProjectPeriod(hr,day,projectperiodnamegiveninclasstt = ["PROJECT"]):
+    if( worksheet1.cell(column=hr, row=day).value in projectperiodnamegiveninclasstt ):
+        return True
+    """Correct ordering here"""
+    elif (hr > 3):
+        return worksheet1.cell(column=hr - 3, row=day).value in projectperiodnamegiveninclasstt
+    elif(hr > 2):
+        return worksheet1.cell(column=hr - 2, row=day).value in projectperiodnamegiveninclasstt
+    elif(hr > 1):
+        return worksheet1.cell(column=hr-1, row=day).value in projectperiodnamegiveninclasstt
+    return False
+def isFreePeriod(hr,day,guideslno=1):
+    return worksheet2.cell(column=hr, row=guideday(day,guideslno) ).value is  None
+def guideday(day,slno=1):
+    #print('##',slno)
+    #print('#',(slno-1)*15 + day )
+    return (slno-1)*15 + day
 FILENAME1 = "_class_tt.xlsx"
 SHEETNAME1 = "Sheet1"
 FILENAME2 = "_guide_tt.xlsx"
@@ -43,10 +60,13 @@ Subject Names
 #    for hr in classhours:
 #        if( worksheet3.cell(column=hr, row=day).value != None ):
 #            print( worksheet3.cell(column=hr, row=day).value )
+print("###",isProjectPeriod(12,3),12,3)
 for day in monfri:
     for hr in classhours:
-        print('#',worksheet1.cell(column=hr, row=day).value)
-        if( worksheet1.cell(column=hr, row=day).value == "Project" ):
-            print(hr,'->',day)
+        #print('#',worksheet1.cell(column=hr, row=day).value)
+        if( isProjectPeriod(hr,day) ):
+            guide_row = [1,16,31,46,61,76,91,106,121,136,151]
+            print(isFreePeriod(hr,day,2),hr,day)
+
             #if(worksheet2.cell(column=hr, row=day).value == None and worksheet2.cell(column=hr, row=day).value == None):
             #    print("Possible")
