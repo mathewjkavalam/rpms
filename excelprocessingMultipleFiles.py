8ace2710f36e8101421611bd7017bab5f45b7512
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2020-01-29 06:55:47 +0530

    Ordering to be implemented

--- excelprocessingMultipleFiles.py
+++ excelprocessingMultipleFiles.py
@@ -2,19 +2,19 @@ from openpyxl import load_workbook
 def isProjectPeriod(hr,day):
     projectperiodnamegiveninclasstt = ["PROJECT"]
     ret = False
-    if( worksheet1.cell(column=hr, row=day).value in projectperiodnamegiveninclasstt ):
+    if( classtt.cell(column=hr, row=day).value in projectperiodnamegiveninclasstt ):
 
         ret =  True
         return ret
     if( hr > 3):
-            ret = worksheet1.cell(column=hr - 3, row=day).value in projectperiodnamegiveninclasstt or ret
+            ret = classtt.cell(column=hr - 3, row=day).value in projectperiodnamegiveninclasstt or ret
     if (hr > 2):
-         ret = worksheet1.cell(column = hr - 2, row=day).value in projectperiodnamegiveninclasstt or ret
+         ret = classtt.cell(column = hr - 2, row=day).value in projectperiodnamegiveninclasstt or ret
     if(hr > 1):
-         ret = worksheet1.cell(column = hr - 1, row=day).value in projectperiodnamegiveninclasstt or ret
+         ret = classtt.cell(column = hr - 1, row=day).value in projectperiodnamegiveninclasstt or ret
     return ret
 def isFreePeriod(hr,day,guideslno=1):
-    return worksheet2.cell(column=hr, row=guideday(day,guideslno) ).value is  None
+    return guidestt.cell(column=hr, row=guideday(day,guideslno) ).value is  None
 def guideday(day,slno=1):
     return (slno-1)*15 + day
 
@@ -54,9 +54,37 @@ workbook2 = load_workbook(filename=FILENAME2)
 classtt = workbook1[SHEETNAME1]
 guidestt = workbook2[SHEETNAME2]
 
+# mon third hour
+day = 1
+hr = 3
+
 workD = { 1:3,2:5,3:7,4:9,5:12}
 classH = {1:3,2:4,3:6,4:7,5:9,6:11,7:12}
-CountOfFacultyCalled = {1:0,2:0,3:1,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
+CountOfFacultyCalled = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
 coordname = "Amitha Mathew"
 guidename = "Amitha Mathew"
 maxpanelcount = 3
+
+"""
+check if coord free 
+"""
+def coordnameSlno(name = "" ):
+    return 1
+print(  isFreePeriod( classH[hr],workD[day],coordnameSlno(name=coordname) ) )
+
+"""
+check if guide free 
+"""
+def guideSlno(name = "" ):
+    return 1
+if( guidename != coordname ):
+    print(  isFreePeriod( classH[hr],workD[day],guideSlno(name=guidename) ) )
+else:
+    print("Guide and Coordinator same")
+"""
+Order remaining faculties
+Priority Criteria:
+    should have free on that hour 
+    arrange ascendingly count called
+    arrange ascendingly count of project hours free for her  
+"""
