61095bb552d6a735ef164e9893c464da7b722ad6
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2020-01-29 06:29:25 +0530

    setup of input done

--- excelprocessingMultipleFiles.py
+++ excelprocessingMultipleFiles.py
@@ -18,25 +18,7 @@ def isFreePeriod(hr,day,guideslno=1):
 def guideday(day,slno=1):
     return (slno-1)*15 + day
 
-FILENAME1 = "_class_tt.xlsx"
-SHEETNAME1 = "Sheet1"
-FILENAME2 = "_guide_tt.xlsx"
-SHEETNAME2 = "Sheet1"
 
-workbook1 = load_workbook(filename=FILENAME1)
-workbook2 = load_workbook(filename=FILENAME2)
-
-
-worksheet1 = workbook1[SHEETNAME1]
-worksheet2 = workbook2[SHEETNAME2]
-
-
-monfri = [3,5,7,9,12]
-
-COLUMN = 1
-ROW = 0
-
-classhours = [3,4,6,7,9,11,12]
 """
 Get Name of Faculty from _guide_tt.xlxs
 print( worksheet2.cell(column=1, row= guideday(3, slno) ).value )
@@ -60,3 +42,21 @@ INPUT:
 OUTPUT:
     panel Members Name
 """
+FILENAME1 = "_class_tt.xlsx"
+SHEETNAME1 = "Sheet1"
+FILENAME2 = "_guide_tt.xlsx"
+SHEETNAME2 = "Sheet1"
+
+workbook1 = load_workbook(filename=FILENAME1)
+workbook2 = load_workbook(filename=FILENAME2)
+
+
+classtt = workbook1[SHEETNAME1]
+guidestt = workbook2[SHEETNAME2]
+
+workD = { 1:3,2:5,3:7,4:9,5:12}
+classH = {1:3,2:4,3:6,4:7,5:9,6:11,7:12}
+CountOfFacultyCalled = {1:0,2:0,3:1,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
+coordname = "Amitha Mathew"
+guidename = "Amitha Mathew"
+maxpanelcount = 3
