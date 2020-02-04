d80657a82dd8686087e684712c608ea4a3fec70a
mathewjkavalam <mathewjkavalam@gmail.com>
2020-02-04 06:31:19 +0530

    timetables standardised

--- excelprocessingMultipleFiles.py
+++ excelprocessingMultipleFiles.py
@@ -45,7 +45,7 @@ OUTPUT:
 """
 FILENAME1 = "_class_tt_org.xlsx"
 SHEETNAME1 = "Sheet1"
-FILENAME2 = "_guide_tt.xlsx"
+FILENAME2 = "_guide_tt_org.xlsx"
 SHEETNAME2 = "Sheet1"
 
 workbook1 = load_workbook(filename=FILENAME1)
@@ -63,7 +63,7 @@ workD = { 1:3,2:5,3:7,4:9,5:12}
 classH = {1:3,2:4,3:6,4:7,5:9,6:11,7:12}
 CountOfFacultyCalled = {1:0,2:0,3:1,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
 coordname = "Amitha Mathew"
-guidename = "Amitha Mathew"
+guidename = "Varghese Paula"
 maxpanelcount = 3
 # print("7879",isProjectPeriod(1,1))
 """
