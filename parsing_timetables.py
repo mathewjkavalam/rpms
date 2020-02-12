d7e9eb4090de7741c1dc87de0132ef255458374b
mathewjkavalam <mathewjkavalam@gmail.com>
2020-02-12 14:37:18 +0530

    fully implemented parsing,including merged handler

--- parsing_timetables.py
+++ parsing_timetables.py
@@ -17,7 +17,7 @@ def parser_faculty_tt():
                 className = facultyTTSheet.cell(row=row,column=col)
                 classLoc = facultyTTSheet.cell(row=row+1,column=col)
                 class_loc.append( (className.value,classLoc.value) )
-                #print(className.value,"<>",classLoc.value,"@",row)
+                # print(className.value,"<>",classLoc.value,"@",row)
 
     return class_loc, len( class_loc )
 
@@ -45,8 +45,9 @@ def parser_class_tt():
     return class_, len( class_ )
 #print( parser_faculty_tt() )
 
-def merged_cell_handler():
-    threelongperiod = ["PROJECT"]
+def merged_cell_handler_faculty_tt():
+    threelongperiod = ["PROJECT","FOSS LAB"]
+    twolongperiod = ["X"]
     #project periods,lab periods
     tt = parser_faculty_tt()[0]
     with open('parser.json') as f:
@@ -54,12 +55,42 @@ def merged_cell_handler():
 
     for fac in range( int(parserConfig["facultycount"]) ):
         for day in range(5):
+            exclude = 0
             for hr in range(7):
-                #3 periods
                 idx = fac*35 + day*7 + hr
-                if( tt[idx] == threelongperiod ):
+                print(tt[idx],fac,day,hr)
+                if( exclude > 0 ):
+                    exclude = exclude - 1
+                elif( tt[idx][0] in threelongperiod and exclude <= 0):
                     tt[idx+1] = tt[idx]
-                    tt[idx+2] = tt[idx]
-                # elif( ):
-                # two long periods
-merged_cell_handler()
+                    tt[idx + 2] = tt[idx]
+                    exclude = 2
+                elif( tt[idx][0] in twolongperiod and exclude <= 0):
+                    tt[idx + 1] = tt[idx]
+                    exclude = 1
+                    pass
+
+    return tt
+def merged_cell_handler_class_tt():
+    threelongperiod = ["PROJECT","FOSS LAB"]
+    twolongperiod = ["X"]
+    #project periods,lab periods
+    tt = parser_class_tt()[0]
+    with open('parser.json') as f:
+      parserConfig = json.load(f)
+
+    for day in range(5):
+        exclude = 0
+        for hr in range(7):
+            idx = day*7 + hr
+            if( exclude > 0 ):
+                exclude = exclude - 1
+            elif( tt[idx] in threelongperiod and exclude <= 0):
+                tt[idx+1] = tt[idx]
+                tt[idx + 2] = tt[idx]
+                exclude = 2
+            elif( tt[idx] in twolongperiod and exclude <= 0):
+                tt[idx + 1] = tt[idx]
+                exclude = 1
+                pass
+    return tt
