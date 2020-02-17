2fd6a98773fc51bfb18f3e7a7c6dca38bcf395ae
mathewjkavalam <mathewjkavalam@gmail.com>
2020-02-17 18:35:27 +0530

    ..

--- New folder/rpms-finale/scheduler.py
+++ New folder/rpms-finale/scheduler.py
@@ -1,13 +1,13 @@
 from scheduling import min_start_date,team,before
-from datetime import timedelta
+import datetime
 
 first_day = min_start_date(team)
 now_day = first_day
 
-while(True):
-
-    print( first_day,next_day )
-    now_day = now_day + timedelta(days=1)
-    for t in team:
-        if before(t["start"],now_day):
-            print( t["start"] )
+now_day = now_day + datetime.timedelta(days=1)
+cursor = 0
+while (cursor < len(team)):
+    if now_day > datetime.datetime.strptime("13-1-2010", '%d-%m-%Y'):
+        # Team is allowed to be allocated by date
+        print(team[cursor])
+    cursor = cursor + 1
